44010c812b447cea1d3f5b57a7d81924be55609a
mohamed elattar [C] <midoelattar20@gmail.com>
2025-01-01 21:11:31 +0200

    calculate age project done

---
+++ Calculate_Age/main.py
@@ -0,0 +1,8 @@
+def main():
+    
+    start_year= int(input("pls enter the start year "))
+    end_year= int(input("pls enter the end year "))
+    age = end_year-start_year
+    print(" your age is ",age)
+
+main()
